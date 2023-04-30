f215511b5ac8f97380f0f0ea33b55c42d9167971
lovelacedrko <114182827+lovelacedrko@users.noreply.github.com>
2023-04-30 09:25:38 -0400

    Create pb class.ino

---
+++ Push button class/pb class.ino
@@ -0,0 +1,18 @@
+// Including the PB header file
+#include "PB.h"
+
+// Creating an instance of the PB class connected to pin 13
+PB button(13);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Check if button is pressed
+  if (button.isPressed()) {
+    // Printing the button state to serial monitor
+    Serial.print("Button state is :  ");
+    Serial.println(digitalRead(13));
+  }
+}
